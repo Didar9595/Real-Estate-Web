ddb8bf3819876e8337f7739eec93aad39adbd864
Your Name <you@example.com>
2023-12-18 15:00:09 +0530

    created signup  api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 mongoose.connect("mongodb://localhost:27017/RealEstate").then(()=>{
     console.log("Database is Connected")
@@ -10,8 +11,12 @@ mongoose.connect("mongodb://localhost:27017/RealEstate").then(()=>{
 
 const app=express()
 
+app.use(express.json())
+
 app.listen(3000,()=>{
     console.log("Server is Running")
 })
 
 app.use("/api/user",userRouter)
+
+app.use('/api/auth',authRouter)
