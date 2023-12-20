6acade2e03dc937d9256c4308418fe9511460e5f
Your Name <you@example.com>
2023-12-20 14:59:01 +0530

    update header and make profile page private to specific user only

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Profile from './pages/Profile'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
   return (
@@ -14,7 +15,10 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
+      
+      <Route element={<PrivateRoute/>}>
       <Route path='/profile' element={<Profile/>}/>
+      </Route>
       <Route path='/signin' element={<Signin/>}/>
       <Route path='/signup' element={<Signup/>}/>
     </Routes>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { AppBar, Button, InputAdornment,Stack, TextField, Toolbar, Typography } from '@mui/material'
+import { AppBar, Avatar, Button, InputAdornment,Stack, TextField, Toolbar, Typography, imageListClasses } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search';
 import {Link} from 'react-router-dom'
+import { useSelector } from 'react-redux';
 
 
 const Header = () => {
+  const {currentUser}=useSelector(state=>state.user)
   return (
     <AppBar position='stick'>
       <Toolbar sx={{ background: '#c5dbfc', height: '10vh' }}>
@@ -16,7 +18,11 @@ const Header = () => {
           <Stack direction='row' spacing={2}>
             <Link to='/'><Button size='large' variant='text' sx={{ fontFamily: 'poppins', color: '#161b21' }}>Home</Button></Link>
             <Link to='/about' > <Button size='large' variant='text' sx={{ fontFamily: 'poppins', color: '#161b21' }}>About</Button></Link>
-            <Link to='/signin'><Button size='large' variant='text'  sx={{ fontFamily: 'poppins', color: '#161b21' }}>Login</Button></Link>
+            <Link to='/profile'>
+              {
+                currentUser?<Avatar src={currentUser.avatar} alt='Profile-pic' width={50} height={50}/>:<Button size='large' variant='text'  sx={{ fontFamily: 'poppins', color: '#161b21' }}>Login</Button>
+              }
+            </Link>
           </Stack>
           <Stack >
             <TextField placeholder='Search...' size='small' InputProps={{ endAdornment: <InputAdornment position='end'><SearchIcon sx={{ color: '#161b21' }} /></InputAdornment> }} sx={{ background: '#dce8fa', border: 'none', borderRadius: '0.4rem' }} />
