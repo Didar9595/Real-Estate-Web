b26c4b6c75c29674a08ca30ca1ae689c11e0fbf7
Your Name <you@example.com>
2023-12-22 12:16:17 +0530

    create image upload logic for the listing

--- frontend/src/pages/CreateListing.jsx
+++ frontend/src/pages/CreateListing.jsx
@@ -1,7 +1,71 @@
-import { Checkbox, Stack, TextField, Typography, FormControlLabel, Button } from '@mui/material'
-import React from 'react'
+import { Checkbox, Stack, TextField, Typography, FormControlLabel, Button, Avatar, IconButton } from '@mui/material'
+import React, { useState } from 'react'
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
+import {app} from '../firebase'
+import ClearIcon from '@mui/icons-material/Clear';
 
 const CreateListing = () => {
+    const [files,setFiles]=useState([])
+    const [formData,setFormData]=useState({
+        imageUrls:[],
+    })
+    const [imageUploadError,setImageUploadError]=useState(false)
+    const [uploading,setUploading]=useState(false)
+
+    console.log(formData)
+    const handleImageSubmit=(e)=>{
+          if(files.length>0 && files.length + formData.imageUrls.length<7){
+            setUploading(true)
+            setImageUploadError(false)
+              const promises=[]
+              for(let i=0;i<files.length;i++){
+                promises.push(storeImage(files[i]))
+              }
+              Promise.all(promises).then((urls)=>{
+                setFormData({...formData,imageUrls:formData.imageUrls.concat(urls)});
+                setImageUploadError(false)
+                setUploading(false)
+              }).catch(error=>{
+                setImageUploadError("Image upload failed (max 2MB per image)")
+                setUploading(false)
+              })
+
+          }
+          else{
+            setImageUploadError("Max 6 Images are allowed")
+            setUploading(false)
+          }
+    }
+
+    const storeImage=async(file)=>{
+        return new Promise((resolve,reject)=>{
+            const storage=getStorage(app)
+            const fileName=new Date().getTime()+file.name
+            const storageRef=ref(storage,fileName)
+            const uploadTask=uploadBytesResumable(storageRef,file)
+
+            uploadTask.on(
+                "state_changed",
+                (snapshot)=>{
+                   const progress=(snapshot.bytesTransferred/snapshot.totalBytes)*100
+                   console.log(`${progress}% is done`)
+                },
+                (error)=>{
+                   reject(error);
+                },
+                ()=>{
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
+                        resolve(downloadURL)
+                    })
+                }
+                )
+        })
+    }
+
+    const handleRemoveImage=(index)=>{
+        setFormData({...formData,imageUrls:formData.imageUrls.filter((url,i)=> i!==index)})
+    }
+    
     return (
         <Stack sx={{ display: 'flex', alignItems: 'center',padding:'1em' }}>
             <Stack direction='column' spacing={3} sx={{ marginTop: '3em', width: '60%' }}>
@@ -36,7 +100,7 @@ const CreateListing = () => {
                 </Stack>
                 </Stack>
                 <Stack direction='row' spacing={3}>
-                <TextField type='number' name='regularPrice' required sx={{ background: 'white' }} />
+                <TextField type='number' name='discountPrice' required sx={{ background: 'white' }} />
                 <Stack direction='column'>
                     <Typography variant='h6' sx={{ fontFamily: 'poppins', fontWeight: 'bold', }}>Discount Price</Typography>
                     <Typography sx={{ fontFamily: 'poppins', fontWeight: 'bold', }}>($/Month)</Typography>
@@ -48,11 +112,30 @@ const CreateListing = () => {
                     <Typography>First Image will be the Cover(max allowed is 6)</Typography>
                 </Stack>
                 <Stack direction='row' spacing={3}>
-                <input type='file' name='images' accept='image/*' multiple style={{border:'0.4px solid #161b21',padding:'1em',width:'70%',fontSize:'18px'}}/>
-                <Button variant='filled' sx={{fontFamily:'poppins',fontWeight:'bold',background:'#161b21',color:'white'}}>Upload</Button>
+                <input onChange={e=>setFiles(e.target.files)} type='file' name='images' accept='image/*' multiple style={{border:'0.4px solid #161b21',padding:'1em',width:'70%',fontSize:'18px'}}/>
+                <Button variant='filled' disabled={uploading} sx={{fontFamily:'poppins',fontWeight:'bold',background:'#161b21',color:'white'}} onClick={handleImageSubmit}>{uploading? 'Uploading...':'Upload'}</Button>
+                </Stack>
+                <Typography variant='body1' color='error' sx={{fontFamily:'poppins',fontWeight:'bold'}}>
+                    {
+                        imageUploadError && imageUploadError
+                    }
+                </Typography>
+               <Stack spacing={3}>
+                {
+                    formData.imageUrls.length>0 && formData.imageUrls.map((url,index)=>(
+                        <Stack direction='row' key={url} sx={{display:'flex',justifyContent:'space-between'}}>
+                            <Avatar src={url} alt='listing-image' variant='rounded' sx={{width:'150px',height:'150px'}}/>
+                            <Stack sx={{display:'flex',justifyContent:'center'}}>
+                            <IconButton color='error' onClick={()=>handleRemoveImage(index)}><ClearIcon fontSize='large'/></IconButton>
+                            </Stack>
+                        </Stack>
+
+                    ))
+                }
                 </Stack>
                 </Stack>
                 <Button variant='contained' sx={{fontFamily:'poppins',fontWeight:'bold',color:'white',background:'#444d5c'}}>Create Listing</Button>
+                
             </Stack>
         </Stack>
     )
