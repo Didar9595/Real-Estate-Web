be507c4fc1bfeb40bc493d00e9bac90e4615f015
Your Name <you@example.com>
2023-12-24 11:36:05 +0530

    done listing page

--- frontend/src/pages/Listing.jsx
+++ frontend/src/pages/Listing.jsx
@@ -5,6 +5,11 @@ import {Swiper,SwiperSlide} from 'swiper/react'
 import SwiperCore from 'swiper'
 import {Navigation} from 'swiper/modules'
 import 'swiper/css/bundle'
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import LocalHotelIcon from '@mui/icons-material/LocalHotel';
+import BathtubIcon from '@mui/icons-material/Bathtub';
+import LocalParkingIcon from '@mui/icons-material/LocalParking';
+import ChairIcon from '@mui/icons-material/Chair';
 
 const Listing = () => {
     SwiperCore.use([Navigation])
@@ -48,11 +53,37 @@ const Listing = () => {
                             {
                                 listing.imageUrls.map(url=>(
                                     <SwiperSlide key={url}>
-                                      <Stack><Avatar variant='square' src={url} alt="listing-images" sx={{width:'100%',height:'550px'}}/></Stack>
+                                      <Stack sx={{padding:'0em 8em'}}><Avatar variant='square' src={url} alt="listing-images" sx={{width:'100%',height:'550px'}}/></Stack>
                                     </SwiperSlide>
                                 ))
                             }
                         </Swiper>
+                        <Stack direction='column' spacing={4} sx={{padding:'3em 8em'}}>
+                           <Typography variant='h4' sx={{fontFamily:'poppins',fontWeight:'bold'}}>
+                            {listing.name} - ${' '}
+                            {listing.offer?listing.discountPrice.toLocaleString('en-US'):listing.regularPrice.toLocaleString('en-US')}
+                            {listing.type==='rent' && '/Month'} 
+                           </Typography>
+                           <Typography sx={{fontFamily:'poppins'}}><LocationOnIcon color='success'/>  {listing.address}</Typography>
+                           <Stack direction='row' spacing={3}>
+                            <Typography sx={{fontFamily:'poppins',padding:'0.5em 4.5em',borderRadius:'0.4em',background:'#fa9bb9',color:'white'}}>{listing.type==='rent'?'For Rent':'For Sale'}</Typography>
+                            {
+                                listing.offer && (
+                                    <Typography sx={{fontFamily:'poppins',padding:'0.5em 4.5em',borderRadius:'0.4em',background:'green',color:'white'}}>${+listing.regularPrice- +listing.discountPrice}</Typography>
+                                )
+                            }
+                           </Stack>
+                           <Stack>
+                            <Typography variant='h5' sx={{fontFamily:'poppins',fontWeight:'bold'}}>Description:</Typography>
+                            {listing.description}
+                           </Stack>
+                           <Stack direction='row' spacing={4}>
+                             <Typography sx={{fontFamily:'poppins',color:'green'}}><LocalHotelIcon color='success'/> {listing.bedrooms>1?`${listing.bedrooms} bedrooms`:`${listing.bedrooms} bedroom`}</Typography>
+                             <Typography sx={{fontFamily:'poppins',color:'green'}}><BathtubIcon color='success'/> {listing.bathrooms>1?`${listing.bathrooms} bathrooms`:`${listing.bathrooms} bathroom`}</Typography>
+                             <Typography sx={{fontFamily:'poppins',color:'green'}}><LocalParkingIcon color='success'/> {listing.parking?"Parking":"No Parking"}</Typography>
+                             <Typography sx={{fontFamily:'poppins',color:'green'}}><ChairIcon color='success'/> {listing.furnished?'Furnished':'Not Furnished'}</Typography>
+                           </Stack>
+                        </Stack>
                     </Stack>
                 )
             }
