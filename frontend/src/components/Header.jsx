6b50020c8682e2d5591a7d179946b4b2fed4e1a0
Your Name <you@example.com>
2023-12-25 15:13:34 +0530

    Update header search bar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { AppBar, Avatar, Button, InputAdornment,Stack, TextField, Toolbar, Typography, imageListClasses } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search';
-import {Link} from 'react-router-dom'
+import {Link,useNavigate} from 'react-router-dom'
 import { useSelector } from 'react-redux';
 
 
 const Header = () => {
   const {currentUser}=useSelector(state=>state.user)
+  const [searchTerm,setSearchTerm]=useState('')
+  const navigate=useNavigate()
+
+  const handleSubmit=()=>{
+     const urlParams=new URLSearchParams(window.location.search)
+     urlParams.set('searchTerm',searchTerm)
+     const searchQuery=urlParams.toString()
+     navigate(`/search?${searchQuery}`);
+  }
+
+  useEffect(()=>{
+    const urlParams=new URLSearchParams(location.search)
+    const searchTermFromUrl=urlParams.get(searchTerm)
+    if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl)
+    }
+  },[location.search])
+
   return (
     <AppBar position='stick'>
       <Toolbar sx={{ background: '#c5dbfc', height: '10vh' }}>
@@ -25,7 +43,7 @@ const Header = () => {
             </Link>
           </Stack>
           <Stack >
-            <TextField placeholder='Search...' size='small' InputProps={{ endAdornment: <InputAdornment position='end'><SearchIcon sx={{ color: '#161b21' }} /></InputAdornment> }} sx={{ background: '#dce8fa', border: 'none', borderRadius: '0.4rem' }} />
+            <TextField placeholder='Search...' size='small' InputProps={{ endAdornment: <InputAdornment position='end'><SearchIcon sx={{ color: '#161b21' }} onClick={handleSubmit}/></InputAdornment> }} sx={{ background: '#dce8fa', border: 'none', borderRadius: '0.4rem' }} value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}/>
           </Stack>
         </Stack>
       </Toolbar>
